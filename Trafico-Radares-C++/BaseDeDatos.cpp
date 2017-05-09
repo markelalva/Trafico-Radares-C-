aa4b84f82fec94f1135cfe3338408bc65de4744b
Markel <Markel@192.168.1.8>
2017-05-09 21:21:18 +0200

    New features

--- Trafico-Radares-C++/BaseDeDatos.cpp
+++ Trafico-Radares-C++/BaseDeDatos.cpp
@@ -7,15 +7,60 @@
 
 #include "BaseDeDatos.h"
 
+
 namespace std {
 
-BaseDeDatos::BaseDeDatos() {
+BaseDeDatos::BaseDeDatos(char *nombre) {
 	// TODO Auto-generated constructor stub
+	this->nombreBD = new char[15];
+	strcpy(this->nombreBD, nombre);
+
 
 }
 
 BaseDeDatos::~BaseDeDatos() {
 	// TODO Auto-generated destructor stub
+	delete[] this->nombreBD;
 }
 
-} /* namespace std */
+int  BaseDeDatos::abrirBD(){
+	int result = sqlite3_open(this->nombreBD, &db);
+	if (result != SQLITE_OK) {
+		cout <<"Error opening database" << endl;
+
+	}
+
+	return result;
+}
+
+int  BaseDeDatos::cerrarBD(){
+	int result = sqlite3_close(db);
+	if (result != SQLITE_OK) {
+		cout<<"Error opening database"<< endl;
+
+	}
+	else{
+
+	cout <<"Database closed" << endl;
+
+	}
+	return result;
+}
+
+int crearTabla(){
+char* orden = "create table Pasos( numeroPaso integer primary key not null, numeroRadar integer not null, matricula text not null, velocidadCoche integer not null)";
+char* error;
+
+int result = sqlite3_exec(db,orden,NULL,0, &error);
+
+if (result != SQLITE_OK){
+	cout << "Error al crear la tabla :    " << error << endl;
+	}else{
+		cout << "Tabla creada \n";
+	}
+	return result;
+}
+
+
+
+ /* namespace std */
--- Trafico-Radares-C++/BaseDeDatos.h
+++ Trafico-Radares-C++/BaseDeDatos.h
@@ -4,19 +4,33 @@
  *  Created on: 8 may. 2017
  *      Author: Markel
  */
-
+#include <string.h>
+#include "sqlite3.h"
+#include <iostream>
 #ifndef BASEDEDATOS_H_
 #define BASEDEDATOS_H_
 
 namespace std {
 
 class BaseDeDatos {
-private:
 	char *nombreBD;
-	sqlite3 *BaseDeDatos;
+	sqlite3 *db;
 	sqlite3_stmt *stmt;
+
+
 public:
-	BaseDeDatos();
+
+	int abrirBD();
+	int cerrarBD();
+
+	int crearTabla();
+
+	void insertPaso();
+	void deletePaso();
+
+	void selectPaso();
+
+	BaseDeDatos(char * nombre);
 	virtual ~BaseDeDatos();
 };
 
