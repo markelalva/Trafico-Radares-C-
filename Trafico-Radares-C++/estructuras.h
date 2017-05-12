5a1f56fb0068c4a5c7b89a5465e2fa522792ecf3
Markel <Markel@192.168.1.2>
2017-05-12 16:24:14 +0200

    Arreglados los pasos

--- Trafico-Radares-C++/estructuras.h
+++ Trafico-Radares-C++/estructuras.h
@@ -7,10 +7,10 @@ typedef struct{
     char nombre[15] ; //Maximo 14 caracteres
     char apellidos [25]; //Apellidos
     char direccion [30];
-    char matricula[8]; //Matricula del coche, se entiende matricula española 9999XXX
+    char matricula[8]; //Matricula del coche, se entiende matricula espaï¿½ola 9999XXX
     char dni [10]; //DNI
     int telefono; //Numero de telefono
-    char contrasenia[15]; //Máximo 14 caracteres
+    char contrasenia[15]; //Mï¿½ximo 14 caracteres
     int tipo; // 0 UsuarioNormal, 1 Administrador
 }Usuario;
 
@@ -18,8 +18,8 @@ typedef struct{
 typedef struct{
     int numeroRadar;
     char nombreRadar[15];
-    int velocidad; //Velocidad de la carretera en la que está el radar
-    double margen; //Margen al que saltará el radar
+    int velocidad; //Velocidad de la carretera en la que estï¿½ el radar
+    double margen; //Margen al que saltarï¿½ el radar
     int activo;  // 0 Desactivado, 1 Activado
 
 
@@ -30,7 +30,6 @@ typedef struct{
 	int numeroRadar;
 	char matricula[8];
 	int velocidadCoche;
-	struct timeb fecha;
 }Paso;
 
 
